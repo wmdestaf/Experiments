421892cd746d7cda35f7cc8497e84106ff1595d0
Will DeStaffan <w55@umbc.edu>
2021-10-29 17:15:02 -0400

    Slight cleanup of clock code. Invalidated / hid new API key properly

--- sunrise_sunset_clock/main.py
+++ sunrise_sunset_clock/main.py
@@ -1,10 +1,16 @@
 import requests as r
 import time
 import math
+import configparser
 from graphics import *
 
 base = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
-key = "66a905986f69f47aa8a8f9eaee03aebb"
+key = "" ##########YOUR KEY GOES HERE#############
+if not key:
+    parser = configparser.ConfigParser()
+    parser.read("../secrets.properties")
+    key = parser.get("sunrise-sunset-clock","key")
+
 clock_sz = 100 #px
 
 #force input
@@ -29,19 +35,19 @@ sst = time.localtime(json['sys']['sunset'])
 win = GraphWin("Sunrise and Sunset", 2 * clock_sz, clock_sz * 1.5)
 
 #clocks
-for clk in ((sun,0),(sst,1)):
+for i, clk in enumerate((sun,sst)):
     height_center = clock_sz/2
-    width_center = height_center + (clk[1] * clock_sz)
+    width_center = height_center + (i * clock_sz)
     hour_arm = height_center * 0.6
     min_arm  = height_center * 0.9
     
     Circle(Point(width_center,height_center), height_center).draw(win)
-    ang = ((clk[0].tm_hour % 12) / 12) * math.tau - (math.pi/2)
+    ang = ((clk.tm_hour % 12) / 12) * math.tau - (math.pi/2)
     Line(Point(width_center,height_center),Point(width_center+(math.cos(ang)*hour_arm), height_center+(math.sin(ang)*hour_arm))).draw(win)
-    ang = (clk[0].tm_min / 60) * math.tau - (math.pi/2)
+    ang = (clk.tm_min / 60) * math.tau - (math.pi/2)
     Line(Point(width_center,height_center),Point(width_center+(math.cos(ang)*min_arm), height_center+(math.sin(ang)*min_arm))).draw(win)
 
-    txt = Text(Point(clock_sz,int(clock_sz*(23/20)) + (2*clock_sz*0.1*clk[1])),time.asctime(clk[0]) + " (EST)")
+    txt = Text(Point(clock_sz,int(clock_sz*(23/20)) + (2*clock_sz*0.1*i)),time.asctime(clk) + " (EST)")
     txt.setSize(int(clock_sz*0.1))
     txt.draw(win)
     

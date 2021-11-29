5838117e8786ef15c91698320d8423bf8428d710
Will DeStaffan <w55@umbc.edu>
2021-11-28 22:56:48 -0500

    Artflow tamper QOL

--- tampers/Artflow.ai/artflow_extend.js
+++ tampers/Artflow.ai/artflow_extend.js
@@ -2,7 +2,7 @@
 // @name         artflow batch
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  improving artflow ux because they refuse ot
+// @description  improving artflow ux because they refuse to
 // @author       wmdestaf
 // @match        https://artflow.ai/
 // @icon         https://www.google.com/s2/favicons?domain=artflow.ai
@@ -10,6 +10,7 @@
 // ==/UserScript==
 var lock;
 var front;
+var most_recent_batch = ["easter egg"];
 (function() {
     'use strict';
 
@@ -19,6 +20,8 @@ var front;
     window.onbeforeunload = function(){
         //save anything as sessionstorage on reload
         localStorage.setItem("batchtext",$("#batch_txt").val())
+        localStorage.setItem("saved",$("#save_txt").val())
+        localStorage.setItem("savetmp",JSON.stringify(most_recent_batch))
     }
 
     
@@ -26,11 +29,20 @@ var front;
     let div = document.getElementsByClassName("input-group-append")[0]
     div.innerHTML += "<button id='batch_btn'>batch</button>"
     div.innerHTML += "<button id='save_btn'>save</button>"
+    div.innerHTML += "<button id='save_qry'>save last</button>"
     div.innerHTML += "<label for='keep_btn'>Do Not Overwrite</label>"
     div.innerHTML += "<input type='checkbox' id='keep_btn'/>"
     $("#keep_btn").change(function() {
         if($(this).prop("checked")) localStorage.setItem("keep_status","true")
         else localStorage.removeItem("keep_status")
+    });
+
+    $(document).on('click', '#save_qry', function() {
+        if(!most_recent_batch) return;
+        let ptr = document.getElementById("save_txt")
+        ptr.value += (ptr.value ? "\n" : "") + most_recent_batch.join("\n")
+        let strs = ptr.value.split("\n")
+        ptr.value = [...new Set(strs)].join("\n")
     });
 
     //save
@@ -45,6 +57,10 @@ var front;
     $("#empty_warning").before("<textarea id='batch_txt' cols='70' rows='12'></textarea>")
     //fill the textfield
     document.getElementById("batch_txt").value = localStorage.getItem("batchtext");
+    //add another one
+    $("#empty_warning").before("<textarea id='save_txt' cols='60' rows='12'></textarea>")
+    document.getElementById("save_txt").value = localStorage.getItem("saved");
+    most_recent_batch = JSON.parse(localStorage.getItem("savetmp"))
 
     //overwrite lock
     $("#keep_btn").prop("checked",localStorage.getItem("keep_status"))
@@ -56,13 +72,15 @@ var front;
         document.title = "Processing: " + (n + 1) + "..."
     }
 
+    const silly_msg = "¡imágenes listas!" //silly but fine
+
     if(localStorage.getItem("done") == 1) {
         localStorage.setItem("done",null)
-        document.title = "¡imágenes listas!" //silly but fine
+        document.title = silly_msg
     }
 
     $(document.documentElement).mousemove(function() {
-        document.title = 'Artflow';
+        if(document.title === silly_msg) document.title = 'Artflow';
     });
 
 
@@ -123,8 +141,8 @@ var front;
        if($("#keep_btn").prop("checked")) el.value = old
        $("#keep_btn").css("disabled","false")
 
+       most_recent_batch = [...new Set(front)]
        lock = 0
        fire(0, front.shift())
     });
-    // Your code here...
 })();
